525d1772403ff6be4c1aee25d94839987f1e7f7c
hunter4983 <132560134+hunter4983@users.noreply.github.com>
2023-11-12 21:39:57 +0300

    qwe

--- file_paths.py
+++ file_paths.py
@@ -6,5 +6,5 @@ file_1_read = default_path_files + "1. Роман И.Ильфа, Е.Петров
 file_2_read = default_path_files + "2. Рассказ И.Ильфа “Москва от зари до зари” Рассказ И.Ильфа “Случай в конторе”.txt"
 file_3_read = default_path_files + "3. Рассказ Е. Петрова “Рассказ об одном солнце” Рассказ Е. Петрова “Семейное счастье” .txt"
 file_4_read = default_path_files + "4. Рассказ М. Булгакова “Я убил” Повесть М. Булгакова “Собачье сердце”, главы 1 и 2.txt"
-
+#
 file_result_write = default_path_results + "result.txt"
