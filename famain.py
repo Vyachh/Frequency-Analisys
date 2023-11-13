4fbc0a4d61ff3dd1c6d3698acbb263d65f1cbb3b
Paco <109136574+Vyachh@users.noreply.github.com>
2023-11-13 16:49:38 +0300

    Auto scanning docs

--- famain.py
+++ famain.py
@@ -1,21 +1,24 @@
 import asyncio
 import falib as fl
 import file_paths as fp
+import fascan as fa
 
 async def main():
     fl.clear_console()
-
-    # Пример текста для анализа
-    filenames = [fp.file_1_read, fp.file_2_read, fp.file_3_read, fp.file_4_read]
-    tasks = [fl.read_file(filename) for filename in filenames]
+    # filenames = [fp.file_1_read, fp.file_2_read, fp.file_3_read, fp.file_4_read]
+    extension = 'txt'
+    file_info = fa.scan_folder(fp.default_path_files, extension)
+    file_names = file_info[0]
+    file_paths = file_info[1]
+    tasks = [fl.read_file(file_path) for file_path in file_paths]
     text = await asyncio.gather(*tasks)
     
     freqs_list = []
-    results_lines = []
-    
-    print("\tЗадание по сдаче курса «Информационные технологии», “Информационные системы”. Анализ данных:\n")
-    fl.start_freq_analyze(filenames, text, freqs_list, results_lines)
-    await fl.switch(filenames, results_lines, freqs_list)
+    results_lines = []    
+
+    print("Задание по сдаче курса «Информационные технологии», “Информационные системы”. Анализ данных:\n")
+    fl.start_freq_analyze(file_names, text, freqs_list, results_lines)
+    await fl.switch(file_names, results_lines, freqs_list)
 
 asyncio.run(main())
 print("Done...")
--- fascan.py
+++ fascan.py
@@ -1,43 +1,71 @@
 import os
 
 def scan_folder(folder_path, extension):
-    file_dict = {}
+    # Инициализация пустых списков для хранения имен файлов и их относительных путей
+    file_names = []
+    file_paths = []
+    
+    # Получение абсолютного пути к текущему скрипту
     script_path = os.path.dirname(os.path.abspath(__file__))
+    
+    # Соединение пути к папке скрипта и указанной папке для сканирования
     folder_abs_path = os.path.join(script_path, folder_path)
     
+    # Обход всех файлов и подпапок в указанной папке
     for root, dirs, files in os.walk(folder_abs_path):
         for file in files:
+            # Проверка, что файл имеет указанное расширение
             if file.endswith('.' + extension):
+                # Формирование полного пути к файлу
                 file_path = os.path.join(root, file)
+                
+                # Вычисление относительного пути от скрипта к файлу
                 rel_path = os.path.relpath(file_path, script_path)
-                file_dict[file] = rel_path
+                
+                # Добавление записей в соответствующие списки
+                file_names.append(file)
+                file_paths.append(rel_path)
     
-    return file_dict
+    # Возврат двух списков с результатами сканирования
+    return file_names, file_paths
 
 folder_path = 'docs\\'
 extension = 'txt'
-files = scan_folder(folder_path, extension)
-print(files)
 
 
 import pymorphy2
 
 # Пример массива freqs_list
-freqs_list = [('красавица', 1),('красивый', 2),( 'красота', 3),( 'красивенький', 4),( 'покрасоваться',5),( 'перекрасить',6),( 'по-любому',4),('бурить', 7),( 'бурильщик',5),( 'буровая',6),( 'бур',7),('буря', 8),( 'бурный',9),( 'буревестник',5)]
+freqs_list = [('красавица', 1), ('красивый', 2), ('красота', 3), ('красивенький', 4), ('покрасоваться', 5),
+              ('перекрасить', 6), ('по-любому', 4), ('бурить', 7), ('бурильщик', 5), ('буровой', 6),
+              ('бур', 7), ('буря', 8), ('бурный', 9), ('буревестник', 5)]
 
-morph = pymorphy2.MorphAnalyzer()
+def process_freqs_list(freqs_list):
+    morph = pymorphy2.MorphAnalyzer()
 
-processed_freqs = {}
+    processed_freqs = {}
 
-for word, freq in freqs_list:
-    # Лемматизируем русское слово
-    lemma = morph.parse(word)[0].normal_form
+    for word, freq in freqs_list:
+    # Получаем нормальную форму слова
+        lemma = morph.parse(word)[0].normal_form
     
-    if lemma in processed_freqs:
-        processed_freqs[lemma] += freq
-    else:
-        processed_freqs[lemma] = freq
+    # Получаем корень слова
+        root = morph.parse(lemma)[0].lexeme[0].word
+    
+    # Если корень уже есть в словаре, увеличиваем его частоту
+        if root in processed_freqs:
+            processed_freqs[root] += freq
+        else:
+        # Если корень отсутствует, добавляем его в словарь
+            processed_freqs[root] = freq
 
-processed_freqs_list = list(processed_freqs.items())
+# Преобразуем словарь в список кортежей
+    processed_freqs_list = list(processed_freqs.items())
+    return processed_freqs_list
 
-print(processed_freqs_list)
+
+
+
+
+
+
